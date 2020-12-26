e2e8b1f2babdb123f1b5ae4a225a7841f28f49a5
Andy <andyzen619@gmail.com>
2020-12-26 01:15:53 -0500

    - add react router

---
+++ src/router.js
@@ -0,0 +1,12 @@
+// handles react router routing
+import React from 'react';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+
+const router = () => {
+  return (<Router>
+    <div></div>
+    <Switch></Switch>
+  </Router>);
+};
+
+export default router;
